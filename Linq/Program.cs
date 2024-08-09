5948b6e5ad0defc12cc435a4c2c51073238cf04b
Rudra Pal <Rudra Pal@DESKTOP-EPCRK2D>
2024-08-09 15:53:19 +0530

    Add project files.

---
+++ Linq/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+    class Program
+    {
+    public static void Main(string[] args)
+    {
+        //int[] age = { 12, 45, 11, 20, 76, 21, 30 };
+        //var demo = from i in age where i > 20 select i;
+        //var demo = from i in age where i > 20 orderby i select i;
+
+        string[] name = { "vipin", "rudra", "kalpana", "Veeru", "Deepak" };
+        // var demo = from a in name where a.StartsWith("v")  select a;
+        var demo = from a in name where a.Contains("v") select a;
+
+        foreach (string item in demo) 
+        {
+            Console.WriteLine(item); 
+        }
+        Console.ReadLine();
+    }
+    }
+
